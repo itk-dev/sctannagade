fbf89f4056cfda2d4193af1f41f5a3d5c0cb2e5f
Jesper Kristensen <jeskr@aarhus.dk>
2010-03-18 11:31:02 +0100

    Added panels

--- sites/all/themes/sctannagade/node-faste_brugere.tpl.php
+++ sites/all/themes/sctannagade/node-faste_brugere.tpl.php
@@ -5,8 +5,12 @@
       </h2>
     <?php endif; ?>
 
+    <?php print $title; ?>
     <?php print $faste_brugere_tabs; ?>
+
+    <div class="boks">
     <?php print $content; ?>
+    </div>
 
     <div class="content-pictures field-field-pictures">
       <?php print views_embed_view('faste_brugere_thumb', 'default', array($nid)); ?>
