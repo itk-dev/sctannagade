0fa6e093f819c511382e87dd45282db370932a90
Jesper Kristensen <jeskr@aarhus.dk>
2010-04-12 13:40:12 +0200

    Fixed "faste brugere" page

--- sites/all/themes/sctannagade/contact-mail-page.tpl.php
+++ sites/all/themes/sctannagade/contact-mail-page.tpl.php
@@ -1,2 +1,29 @@
-<p><?php print t('You can also contact Sct. Anna Gade via the contact form below.'); ?></p>
-<div class="submit-contact-form"><?php print $form_markup; ?></div>
+<div class="kontakt">
+
+	<h1>Kontakt sct. anna gade</h1><br />
+	<strong>Sct. Annagade</strong>
+	<p>
+	Skt. Anna Gade 38 - 40<br/>
+	8000 Århus C<br />
+	<br />
+	Telefon 8613 4666<br />
+	Mobiltelefon 2425 5866<br />
+	<br/>
+	Daglig leder Steen Thuesen,<br />
+	Teknisk servicemedarbejder Torben Sandnes Bjørnhaug,<br />
+	Teknisk servicemedarbejder Bent Hansen.<br/>
+	<br />
+	Dagligt: 07.00-22.00<br />
+	Lørdage: 09.00-21.00<br />
+	Søndage: 09.00-19.00<br />
+	</p>
+
+</div>
+<div class="kontakt-form">
+
+	<p><?php print t('You can also contact Sct. Anna Gade via the contact form below.'); ?></p>
+	<div class="submit-contact-form">
+		<?php print $form_markup; ?>
+	</div>
+
+</div>
